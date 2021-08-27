8b6cb1de15c918667ae9e5ddbfa2e3230ba3a733
sivaramkumar_ <sivaramkumar925@gmail.com>
2021-08-27 22:09:03 +0530

    signin module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import {GlobalStyle} from './global-styles';
+import { GlobalStyle } from './global-styles';
 import 'normalize.css';
-import {firebase} from './libs/firebase.prod'
+import { firebase } from './libs/firebase.prod'
+import { FirebaseContext } from './context/firebase';
 
 ReactDOM.render(
     <>
-    <GlobalStyle/>
-    <App/>
-    </>,    
+        <FirebaseContext.Provider value={{ firebase }}>
+            <GlobalStyle />
+            <App />
+        </FirebaseContext.Provider>
+    </>,
     document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
